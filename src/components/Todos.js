495281d84a1c086adca9c4f3890292d21196c451
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-11 16:19:10 +0100

    Update

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,6 +3,21 @@ import './Todos.css';
 
 export const Todos = () => {
   const [todos, setTodos] = useState([]);
+  const [isAllCompleted, setIsAllCompleted] = useState(false);
+  const [percentage, setPercentage] = useState(0);
+
+  const onChangeIsComplete = (id) => {
+    const updatedIsComplete = todos.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          completed: !item.completed,
+        };
+      };
+      return item;
+    });
+    setTodos(updatedTodos);
+  };
 
   useEffect(() => {
     fetch('https://dummyjson.com/todos')
@@ -12,17 +27,26 @@ export const Todos = () => {
       });
   }, []);
 
+  useEffect(() => {
+    const isAllCompleted = todos.every((item) => item.completed).length;
+    if (!completedTodos || !todos.length) return setPercentage(0);
+    const percentage = (completedTodos / todos.length) * 100;
+    setPercentage(percentage);
+  }, [todos]);
+
   return (
     <div className="todos-wrapper">
       <h1>Todos</h1>
+      <h4>Procenat % uradjenih taskova: {percentage.toFixed(0)}%</h4>
       {todos.map((item) => {
         return (
           <div key={item.id} className="todo">
-            <input type="checkbox" checked={item.completed} />
+            <input type="checkbox" checked={item.completed} onChange={() => onChangeIsComplete(item.id)}/>
             <span>{item.todo}</span>
           </div>
         );
       })}
+      {isAllCompleted && <h3>All items are checked</h3>}
     </div>
   );
 };
