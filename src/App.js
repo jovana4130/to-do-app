30f89f21e1f387f2b396936090e0906537653c98
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-11 16:51:16 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
+import './components/Todos.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-        </a>
-      </header>
+      <header className="App-header"></header>
       <body>
         <h1>TO DOs</h1>
-        
+
       </body>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Todos from './components/Todos';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App />
     <Todos />
   </React.StrictMode>
 );
